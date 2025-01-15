7b9e80006b66a15e3a6d306e825fa0715e19a0ef
jorgenmjobloop <jorgen@jobloop.no>
2025-01-15 15:44:20 +0100

    added utility scripts and the peer-2-peer class

---
+++ Util/scripts/ShellScript.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+#!/usr/bin/bash
+
+import argparse
+import sys
+import os
+import subprocess
+import time
+import pathlib
+
+def get_standard_output():
+    with open("../Internal/expected_output", "r") as f:
+        print(f.read()) # verify that this is the correct output, and pass it to the run function
+
+
+def run():
+    args = ["cd", "dotnet","cron", "./", "sudo", "apt-get","update", "upgrade", "install -y"]
+    get_stdout = get_standard_output()
+    expected_output = subprocess.Popen(["dotnet", "--version"]) # expected output from stdout
+    if(expected_output != get_stdout):
+       print("Installing depencies..")
+       subprocess.Popen(["chmod+=x", "dotnet.sh"])
+       subprocess.Popen("./dotnet.sh") # must run as root
+    subprocess.Popen("dotnet watch --quiet run")
+    print("Server checking for dependencies, please wait..")
+    time.sleep(10)
+    print("Finished checking depencies, running application..")
+    subprocess.Popen(args[1] + "watch --quiet run", shell=True)    
+
---
+++ Util/scripts/dotnet.sh
@@ -0,0 +1,6 @@
+#!usr/bin/bash
+
+# this needs to run with sudo permissions
+
+sudo apt-get install -y dotnet-runtime-8.0
+
